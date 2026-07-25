4eff932c51003d005bc980159adfff4cc5f421ef
l1teSG <juanfelipe123leo@gmail.com>
2026-07-25 21:58:13 +0000

    feat: add the shoot

--- dirty-road/ecenes/players/quark/quark.gd
+++ dirty-road/ecenes/players/quark/quark.gd
@@ -1,6 +1,21 @@
 extends Player
 
-@onready var rangeArea = $range/rangeArea
+@onready var rangeArea = $Area2D/range
+var proyectil = preload("res://ecenes/players/projectile/quark/proyectilQuark.tscn")
+var power = 'basic' #basic super mega
 
-func _init() -> void:
-	print(rangeArea)
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("🔵 body_entered disparado. Cuerpo: ", body.name)
+	if body.is_in_group('enemi'):
+		var direction = (body.global_position - self.global_position).normalized()
+		shot(direction, power)
+
+func shot(direction, power):
+	
+	var bullet = proyectil.instantiate()
+	bullet.positionEnemi = direction
+	self.add_child(bullet) 
+	
+
+func _ready() -> void:
+	pass
