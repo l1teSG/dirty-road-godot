b306d365c9f8ce14f80d0504ba1a72a8a55c4839
l1teSG <juanfelipe123leo@gmail.com>
2026-07-27 14:36:22 +0000

    feat: add the powers

--- dirty-road/ecenes/players/quark/quark.gd
+++ dirty-road/ecenes/players/quark/quark.gd
@@ -3,21 +3,30 @@ extends Player
 @onready var rangeArea = $Area2D/range
 var proyectil = preload("res://ecenes/players/projectile/quark/proyectilQuark.tscn")
 var power = 'basic' #basic super mega
+var onFire: bool = false
+var direction: Vector2 = Vector2.ZERO
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('enemi'):
-		var direction = (body.global_position - self.global_position).normalized()
+		direction = (body.global_position - self.global_position).normalized()
 		shot(direction, power)
 
 func shot(direction, power):
-	var bullet = proyectil.instantiate()
-	bullet.global_position = $aim.global_position
-	bullet.positionEnemi = direction
-	get_tree().root.add_child(bullet)
+	if power == "basic":
+		var bullet = proyectil.instantiate()
+		bullet.global_position = $aim.global_position
+		bullet.positionEnemi = direction
+		get_tree().root.add_child(bullet)
 
-func _ready() -> void:
-	pass
 
 
 func _on_timer_timeout() -> void:
 	pass
+
+
+func _on_super_pressed() -> void:
+	power = 'super'
+
+
+func _on_mega_pressed() -> void:
+	power = 'mega'
