24eaff86fa714ad594c106d57f1273d14f0de6ac
l1teSG <juanfelipe123leo@gmail.com>
2026-07-27 14:42:57 +0000

    fix: the automatic fire

--- dirty-road/ecenes/players/quark/quark.gd
+++ dirty-road/ecenes/players/quark/quark.gd
@@ -4,12 +4,14 @@ extends Player
 var proyectil = preload("res://ecenes/players/projectile/quark/proyectilQuark.tscn")
 var power = 'basic' #basic super mega
 var onFire: bool = false
+var OnEnemiRange: bool = false
 var direction: Vector2 = Vector2.ZERO
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('enemi'):
+		OnEnemiRange = true
 		direction = (body.global_position - self.global_position).normalized()
-		shot(direction, power)
+		
 
 func shot(direction, power):
 	if power == "basic":
@@ -21,7 +23,8 @@ func shot(direction, power):
 
 
 func _on_timer_timeout() -> void:
-	pass
+	if power == 'basic' and OnEnemiRange:
+		shot(direction, power)
 
 
 func _on_super_pressed() -> void:
