79b8ab249be58881810e82bb68536b2a28acf30f
l1teSG <juanfelipe123leo@gmail.com>
2026-07-28 00:21:47 +0000

    fix: the aim

--- dirty-road/ecenes/players/quark/quark.gd
+++ dirty-road/ecenes/players/quark/quark.gd
@@ -4,30 +4,28 @@ extends Player
 var proyectil = preload("res://ecenes/players/projectile/quark/proyectilQuark.tscn")
 var power = 'basic' #basic super mega
 var onFire: bool = false
-var OnEnemiRange: bool = false
-var direction: Vector2 = Vector2.ZERO
+var enemi: Node
+
+
+func shot(enemi, power):
+	if onFire:
+		var direction = (enemi.global_position - $aim.global_position).normalized()
+		var bullet = proyectil.instantiate()
+		bullet.add_to_group('bullet')
+		bullet.global_position = $aim.global_position
+		bullet.positionEnemi = direction
+		get_tree().current_scene.add_child(bullet)
+		print('enemigo en rango ')
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('enemi'):
-		OnEnemiRange = true
-		direction = (body.global_position - self.global_position).normalized()
-	else:
-		OnEnemiRange = false
-
-func shot(direction, power):
-	if power == "basic":
-		var bullet = proyectil.instantiate()
-		bullet.global_position = $aim.global_position
-		bullet.positionEnemi = direction
-		bullet.add_to_group('fire_player')
-		get_tree().root.add_child(bullet)
+		onFire = true
+		enemi = body
 
 
 
 func _on_timer_timeout() -> void:
-	if power == 'basic' and OnEnemiRange:
-		shot(direction, power)
-
+	shot(enemi, power)
 
 func _on_super_pressed() -> void:
 	power = 'super'
@@ -38,3 +36,9 @@ func _on_mega_pressed() -> void:
 
 func take_damage(damage):
 	pass
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group('enemi'):
+		onFire = false
+	
