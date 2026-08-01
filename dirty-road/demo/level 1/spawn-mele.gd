1b34000c995ce0cecb283360807f04b35a31c723
l1teSG <juanfelipe123leo@gmail.com>
2026-08-01 21:39:31 +0000

    feature: add the save game

--- dirty-road/demo/level 1/spawn-mele.gd
+++
@@ -1,9 +0,0 @@
-extends Marker2D
-
-var enemi = preload('res://assets/stage/elDespertarDeLasMaquinas/enemies/Carbonilla/shapeCarbonilla.tscn')
-
-func _on_timer_timeout() -> void:
-	var enemiInstancia = enemi.instantiate()
-	enemiInstancia.global_position = self.global_position
-	enemiInstancia.add_to_group('enemi')
-	get_tree().current_scene.add_child(enemiInstancia)
